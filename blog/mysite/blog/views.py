a905b18b8b7f6a1fb1acb99be1f7a3bc74d139bc
yunskim <yeonseung.kim@gmail.com>
2020-07-02 09:49:02 +0900

    classview로 대체

--- blog/mysite/blog/views.py
+++ blog/mysite/blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Post
 
@@ -34,4 +35,11 @@ def post_detail(request, year, month, day, post):
                   'blog/post/detail.html',
                   {'post': post})
 
+
+class PostListView(ListView):
+
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 2
+    template_name = 'blog/post/list.html'
 
