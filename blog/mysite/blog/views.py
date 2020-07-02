3d7265272e093b665ac0ccb2c19da608f3487acd
yunskim <yeonseung.kim@gmail.com>
2020-07-02 09:42:10 +0900

    pagination 추가

--- blog/mysite/blog/views.py
+++ blog/mysite/blog/views.py
@@ -1,14 +1,27 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Post
 
 # Create your views here.
 
+
 def post_list(request):
 
-    posts = Post.draft.all()
+    object_list = Post.published.all()
+    paginator = Paginator(object_list, 2)
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+
     return render(request,
                   'blog/post/list.html',
-                  {'posts': posts})
+                  {'page': page,
+                   'posts': posts})
+
 
 def post_detail(request, year, month, day, post):
 
@@ -20,4 +33,5 @@ def post_detail(request, year, month, day, post):
     return render(request,
                   'blog/post/detail.html',
                   {'post': post})
+
 
